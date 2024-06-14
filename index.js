21fa7048fdbbc5bc9918aa1e9bd8e4e429346fd4
sazep <sazep7w@gmail.com>
2024-06-14 15:55:18 +0200

    coppy main

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 fetch("/data/new.json")
-    .then((res) => res.json())
-    .then((data) =>{
-        const temp = $('#book').html();
-        const hb_temp = Handlebars.compile(temp);
-        $("#new").html(hb_temp(data));
-    });
-
-// fetch("/data/popular.json")
-//     .then((res) => res.json())
-//     .then((data) =>{
-//         const temp = $('#popular').html();
-//         const hb_temp = Handlebars.compile(temp);
-//         $("#new").html(hb_temp(data));
-//     });
+    .then((res)=>res.json())
+    .then((dta)=>{
+        const temp = $("#book").html()
+        const htTemp = Handlebars.compile(temp)
+        $("#new").html(htTemp(dta))
+    })
