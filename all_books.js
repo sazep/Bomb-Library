4fe8033a746be55c5f28873a5f05ae7737c1dd0c
Gooogasol <egorshevchuk6a@gmail.com>
2024-06-21 18:08:09 +0300

    git white

--- all_books.js
+++ all_books.js
@@ -1,15 +1,76 @@
 const count = 3
 let page = 0
+let all_books
 function load_books(){
     fetch("/data/data.json")
         .then((res) => res.json())
         .then((data) => {
+            all_books = data.length
             let s = page*count 
             let n = s+3
             data = data.slice(s,n)
             const temp = $('#onebook').html();
             const hb_temp = Handlebars.compile(temp);
             $("#allbooks").html(hb_temp(data));
+            document.querySelector('.page').innerHTML = page+1
+
+        })
+        .then((aboba) =>{
+            let books = document.querySelectorAll('.onebook')
+            for(let bb of books){
+                bb.addEventListener('click', function(){
+                    let id = this.querySelector('.id').innerHTML    
+                    localStorage.setItem('id',id)
+                    window.location.href='../html/about book.html'
+                })
+            }
         })
 }
 load_books()
+
+let before = document.querySelector('.before')
+let next = document.querySelector('.next')
+
+next.addEventListener('click', ()=>{
+    if (page*count < all_books) {
+        page = page + 1
+        load_books()
+        before.style.zIndex = 0
+    } 
+    if ((page+1)*count > all_books){
+        next.style.display = 'none'
+    }
+
+})
+
+before.addEventListener('click', ()=>{
+    if (page > 0) {
+        page = page - 1
+        load_books()
+        next.style.display = 'flex'
+    } if (page == 0){
+        before.style.zIndex = -1
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 fetch("/data/new.json")
-    .then((res) => res.json())
-    .then((data) => {
-        const temp = $('#book').html();
-        const hb_temp = Handlebars.compile(temp);
-        $("#new").html(hb_temp(data));
+    .then((res)=>res.json())
+    .then((data)=>{
+        const temp = $("#book").html()
+        const htTemp = Handlebars.compile(temp)
+        $("#new").html(htTemp(data))
     })
-    .catch((error) => console.error('Error:', error));
 
 fetch("/data/popular.json")
-    .then((res) => res.json())
-    .then((data) => {
-        const temp = $('#book').html();
-        const hb_temp = Handlebars.compile(temp);
-        $("#popular").html(hb_temp(data));
+    .then((res)=>res.json())
+    .then((data)=>{
+        const temp = $("#book").html()
+        const htTemp = Handlebars.compile(temp)
+        $("#popular").html(htTemp(data))
     })
-    .catch((error) => console.error('Error:', error));
+
+const slides = document.querySelector('.slides');
+const slideImages = document.querySelectorAll('.slide');
+let currentIndex = 0;
+const intervalTime = 5000
+
+function showSlide(index) {
+    if (index >= slideImages.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slideImages.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+slideImages.forEach(slide => {
+    slide.addEventListener('click', () => {
+        const link = slide.getAttribute('data-link');
+        window.location.href = link;
+    });
+});
+
+function startSlider() {
+    setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, intervalTime);
+}
+
+showSlide(currentIndex);
+startSlider();
