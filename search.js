a364d8b5a48a6757760d35a7939709e4641fefdd
sazep <sazep7w@gmail.com>
2024-06-29 20:41:55 +0200

    1

--- search.js
+++ search.js
@@ -5,7 +5,7 @@ let filter_book
 $("#search").on("keyup", function (event) {
     if (event.key === "Enter" || event.keyCode === 13) {
         let txt = input.value
-        fetch("/data/data.json")
+        fetch("../data/data.json")
             .then((res) => res.json())
             .then((data) => {
                 filter_book = []
@@ -15,8 +15,7 @@ $("#search").on("keyup", function (event) {
                     }
                 }
                 localStorage.setItem("filterbook", JSON.stringify(filter_book))
-                window.location.href = "/html/filter_book.html"
+                window.location.href = "../html/filter_book.html"
             })
     }
 })
-
